fd928469e7959e500922ba5f5807a772f5461c19
Van Phu <phuphupu5@gmail.com>
2023-03-23 15:34:30 +0700

    loading meo co % vi kho qua

--- Quan_li_vat_tu/Loading.h
+++ Quan_li_vat_tu/Loading.h
@@ -15,15 +15,15 @@ void DrawBar(int x1, int y1, int x2, int y2, int maunen) {
 	bar(x1, y1, x2, y2);
 }
 
-void DrawLoading(int x1, int y1, int x2, int y2) {
-	DrawRecTangle(x1, y1, x2, y2, RED, 2);
+void DrawLoading(int x1, int y1, int x2, int y2,int t) {
+	//DrawRecTangle(x1, y1, x2, y2, RED, 2);
 	int kc = 20;
 	int i = 1;
 	int j = x1;
 	while ((x1 + (i - 1) * kc) < x2) {
-		DrawBar(x1 + 3, y1 + 3, x1 + kc * i - 18, y2 - 3, RED);
+		DrawBar(x1, y1 + 3, x1 + kc * i , y2 - 3, RED);
 		i++;
-		delay(15);
+		delay(t);
 	}
 }
 
@@ -34,20 +34,91 @@ void Loading(int& x, int& y)
 	text_box_no_border(200, 240, 300, 340, (char*)"DO NGUYEN PHUONG", 4, 2, 0, 0, CLL, 0);
 	text_box_no_border(200, 280, 300, 380, (char*)"HO VAN PHU", 4, 2, 0, 0, CLL, 0);
 	text_box_no_border(600, 350, 900, 390, (char*)"GV: LUU NGUYEN KY THU", 4, 2, 0, 8, CLL, 0);
-	text_box_no_border(480, 450, 700, 490, (char*)"BAT DAU", 4, 4, 0, 8, COLOR(240, 89, 179), 0);
+	text_box(480, 470, 700, 510, (char*)"BAT DAU", 4, 4, 5, 8, COLOR(240, 89, 179), 0);
 	while (1)
 	{
 		batdau:
 		if (ismouseclick(WM_LBUTTONDOWN))
 		{
 			getmouseclick(WM_LBUTTONDOWN, x, y);
-			if (ktVT(480, 450, 700, 490, x, y))
+			if (ktVT(480, 470, 700, 510, x, y))
 			{
-				text_box_no_border(480, 450, 700, 500, (char*)"", 4, 5, 0, 8, CLL, 0);
-				text_box_no_border(250, 400, 300, 420, (char*)"Loading...", 4, 1, 0, 8, CLL, 0);
-				DrawLoading(200, 420, 985, 450);
-				//rectangle(200, 420, 985, 450);
+				//lan 1
+				DrawRecTangle(200, 420, 985, 450, RED, 2);
+				text_box_no_border(480, 470, 700, 510, (char*)"", 4, 5, 0, 8, CLL, 0);
+				text_box_no_border(250, 390, 300, 410, (char*)"Loading.", 4, 1, 0, 8, CLL, 0);
+				DrawLoading(200, 420, 278.5, 450, 10);
+				delay(200);
+				text_box_no_border(250, 390, 300, 410, (char*)"Loading..", 4, 1, 0, 8, CLL, 0);
+				delay(200);
+				text_box_no_border(250, 390, 300, 410, (char*)"Loading...", 4, 1, 0, 8, CLL, 0);
+				delay(200);
 
+				// lan 2
+				
+				text_box_no_border(250, 390, 300, 410, (char*)"Loading...", 4, 1, 0, 8, CLL, CLL);
+				text_box_no_border(250, 390, 300, 410, (char*)"Loading.", 4, 1, 0, 8, CLL, 0);
+				delay(200);
+				text_box_no_border(250, 390, 300, 410, (char*)"Loading..", 4, 1, 0, 8, CLL, 0);
+				delay(200);
+				text_box_no_border(250, 390, 300, 410, (char*)"Loading...", 4, 1, 0, 8, CLL, 0);
+				delay(200);
+				DrawLoading(278.5, 420, 592.5, 450, 15);
+				
+				//lan 3
+				text_box_no_border(250, 390, 300, 410, (char*)"Loading...", 4, 1, 0, 8, CLL, CLL);
+				text_box_no_border(250, 390, 300, 410, (char*)"Loading.", 4, 1, 0, 8, CLL, 0);
+				delay(200);
+				text_box_no_border(250, 390, 300, 410, (char*)"Loading..", 4, 1, 0, 8, CLL, 0);
+				delay(200);
+				text_box_no_border(250, 390, 300, 410, (char*)"Loading...", 4, 1, 0, 8, CLL, 0);
+				delay(200);
+
+				text_box_no_border(250, 390, 300, 410, (char*)"Loading...", 4, 1, 0, 8, CLL, CLL);
+				text_box_no_border(250, 390, 300, 410, (char*)"Loading.", 4, 1, 0, 8, CLL, 0);
+				delay(200);
+				text_box_no_border(250, 390, 300, 410, (char*)"Loading..", 4, 1, 0, 8, CLL, 0);
+				delay(200);
+				text_box_no_border(250, 390, 300, 410, (char*)"Loading...", 4, 1, 0, 8, CLL, 0);
+				delay(200);
+
+				DrawLoading(592.5, 420, 906.5, 450, 15);
+
+				//lan 4
+				text_box_no_border(250, 390, 300, 410, (char*)"Loading...", 4, 1, 0, 8, CLL, CLL);
+				text_box_no_border(250, 390, 300, 410, (char*)"Loading.", 4, 1, 0, 8, CLL, 0);
+				delay(200);
+				text_box_no_border(250, 390, 300, 410, (char*)"Loading..", 4, 1, 0, 8, CLL, 0);
+				delay(200);
+				text_box_no_border(250, 390, 300, 410, (char*)"Loading...", 4, 1, 0, 8, CLL, 0);
+				delay(200);
+
+				text_box_no_border(250, 390, 300, 410, (char*)"Loading...", 4, 1, 0, 8, CLL, CLL);
+				text_box_no_border(250, 390, 300, 410, (char*)"Loading.", 4, 1, 0, 8, CLL, 0);
+				delay(200);
+				text_box_no_border(250, 390, 300, 410, (char*)"Loading..", 4, 1, 0, 8, CLL, 0);
+				delay(200);
+				text_box_no_border(250, 390, 300, 410, (char*)"Loading...", 4, 1, 0, 8, CLL, 0);
+				delay(200);
+
+				text_box_no_border(250, 390, 300, 410, (char*)"Loading...", 4, 1, 0, 8, CLL, CLL);
+				text_box_no_border(250, 390, 300, 410, (char*)"Loading.", 4, 1, 0, 8, CLL, 0);
+				delay(200);
+				text_box_no_border(250, 390, 300, 410, (char*)"Loading..", 4, 1, 0, 8, CLL, 0);
+				delay(200);
+				text_box_no_border(250, 390, 300, 410, (char*)"Loading...", 4, 1, 0, 8, CLL, 0);
+				delay(200);
+
+				text_box_no_border(250, 390, 300, 410, (char*)"Loading...", 4, 1, 0, 8, CLL, CLL);
+				text_box_no_border(250, 390, 300, 410, (char*)"Loading.", 4, 1, 0, 8, CLL, 0);
+				delay(200);
+				text_box_no_border(250, 390, 300, 410, (char*)"Loading..", 4, 1, 0, 8, CLL, 0);
+				delay(200);
+				text_box_no_border(250, 390, 300, 410, (char*)"Loading...", 4, 1, 0, 8, CLL, 0);
+				delay(200);
+
+				DrawLoading(906.5, 420, 985, 450, 15);
+				delay(500);
 			}
 			else
 				goto batdau;
