a32581d6a993a9a3268a0ab93b8ccc3e885c73df
Van Phu <phuphupu5@gmail.com>
2023-04-25 14:32:00 +0700

    them 4 mavt = fale

--- Quan_li_vat_tu/Bill_Func.h
+++ Quan_li_vat_tu/Bill_Func.h
@@ -1580,7 +1580,7 @@ nhd:
 				text_box_no_border(20, 170, 100, 210, (char*)"X", f_medium, 3, 10, 30, 9);
 				text_box_no_border(20, 130, 100, 170, (char*)"N", f_medium, 3, 10, 30, 14, 0);
 				khung_b_nhd();
-				nhap = true; xuat = false;
+				nhap = true; xuat = false; mavt = false;
 				continue;
 
 			}
@@ -1590,7 +1590,7 @@ nhd:
 				text_box_no_border(20, 130, 100, 170, (char*)"N", f_medium, 3, 10, 30, 9);
 				text_box_no_border(20, 170, 100, 210, (char*)"X", f_medium, 3, 10, 30, 14, 0);
 				khung_b_nhd();
-				nhap = false; xuat = true;
+				nhap = false; xuat = true; mavt = false;
 				continue;
 			}
 			if (ktVT(110, 120, 590, 145, x, y))
@@ -1778,7 +1778,8 @@ nhd:
 						text_box_no_border(20, 170, 100, 210, (char*)"X", f_medium, 3, 10, 30, 9);
 						text_box_no_border(20, 130, 100, 170, (char*)"N", f_medium, 3, 10, 30, 14, 0);
 						khung_b_nhd();
-						nhap = true; xuat = false;
+						nhap = true; xuat = false;mavt = false;
+						
 						continue;
 					}
 					if (ktVT(20, 170, 100, 210, x, y))
@@ -1787,7 +1788,7 @@ nhd:
 						text_box_no_border(20, 130, 100, 170, (char*)"N", f_medium, 3, 10, 30, 9);
 						text_box_no_border(20, 170, 100, 210, (char*)"X", f_medium, 3, 10, 30, 14, 0);
 						khung_b_nhd();
-						nhap = false; xuat = true;
+						nhap = false; xuat = true;mavt = false;
 						continue;
 					}
 				}
